f87f50662c85fcbc77236a7e559565e404679dc1
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-26 00:42:44 +0530

    Check Different Emails

--- CheckDiffEmail.cs
+++ CheckDiffEmail.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace ReGex_UserRegistration
+namespace UserRegistration
 {
     class CheckDiffEmail
     {
